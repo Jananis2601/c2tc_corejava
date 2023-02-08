4caa4b78820de93fd81fa16945a0b10b61a5c086
jananis26 <jananis27@gmail.com>
2023-02-08 13:56:09 +0530

    First commit

---
+++ JDBC/src/jdbcexapmle/Jdbcdemo.java
@@ -0,0 +1,10 @@
+package jdbcexapmle;
+
+public class Jdbcdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JDBC/src/project/projectdemo.java
@@ -0,0 +1,10 @@
+package project;
+
+public class projectdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
